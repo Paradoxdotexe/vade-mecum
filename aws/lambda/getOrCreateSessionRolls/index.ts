6b55b817d8e2759ba20f67b3c9617b9437670976
Nathan Klingensmith <paradoxpyt@gmail.com>
2024-04-13 15:44:16 -0400

    PATCH: Post roll to DB

--- aws/lambda/getOrCreateSessionRolls/index.ts
+++ aws/lambda/getOrCreateSessionRolls/index.ts
@@ -1,13 +1,13 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import { DynamoDBDocumentClient, QueryCommand } from '@aws-sdk/lib-dynamodb';
+import { DynamoDBDocumentClient, QueryCommand, PutCommand } from '@aws-sdk/lib-dynamodb';
 
 const client = new DynamoDBClient({});
 const docClient = DynamoDBDocumentClient.from(client);
 
 const RESPONSE_HEADERS = {
   'Content-Type': 'application/json',
-  'Access-Control-Allow-Methods': 'GET'
+  'Access-Control-Allow-Methods': 'GET,POST'
 };
 
 const ALLOWED_ORIGINS = ['http://localhost:3000', 'https://vademecum.thenjk.com'];
@@ -32,29 +32,62 @@ const handler: APIGatewayProxyHandler = async event => {
 
   const sessionId = event.pathParameters!['sessionId'];
 
-  // get all "roll" items for a specific session
-  const queryCommand = new QueryCommand({
-    TableName: 'vade-mecum-sessions',
-    KeyConditionExpression: 'sessionId = :sessionId and begins_with(itemId, :itemIdPrefix)',
-    ExpressionAttributeValues: {
-      ':sessionId': sessionId,
-      ':itemIdPrefix': 'roll'
-    },
-    ExpressionAttributeNames: {
-      '#timestamp': 'timestamp'
-    },
-    ProjectionExpression: 'itemId, #timestamp, evaluation, dice, label, characterId'
-  });
+  if (event.httpMethod === 'GET') {
+    // get all "roll" items for a specific session
+    const queryCommand = new QueryCommand({
+      TableName: 'vade-mecum-sessions',
+      KeyConditionExpression: 'sessionId = :sessionId and begins_with(itemId, :itemIdPrefix)',
+      ExpressionAttributeValues: {
+        ':sessionId': sessionId,
+        ':itemIdPrefix': 'roll'
+      },
+      ExpressionAttributeNames: {
+        '#timestamp': 'timestamp'
+      },
+      ProjectionExpression: 'itemId, #timestamp, evaluation, dice, label, characterId'
+    });
 
-  const response = await docClient.send(queryCommand);
+    const response = await docClient.send(queryCommand);
 
-  const rolls = response.Items?.map(formatRoll) ?? [];
+    const rolls = response.Items?.map(formatRoll) ?? [];
 
-  return {
-    statusCode: 200,
-    headers: RESPONSE_HEADERS,
-    body: JSON.stringify(rolls)
-  };
+    return {
+      statusCode: 200,
+      headers: RESPONSE_HEADERS,
+      body: JSON.stringify(rolls)
+    };
+  } else {
+    if (!event.body) {
+      return {
+        statusCode: 400,
+        headers: RESPONSE_HEADERS,
+        body: JSON.stringify({ detail: 'Missing request body.' })
+      };
+    }
+
+    const roll = JSON.parse(event.body);
+
+    const putCommand = new PutCommand({
+      TableName: 'vade-mecum-sessions',
+      Item: {
+        sessionId: sessionId,
+        itemId: `roll#${roll.id}`,
+        characterId: roll.characterId,
+        label: roll.label,
+        dice: roll.dice,
+        timestamp: roll.timestamp,
+        evaluation: roll.evaluation
+      }
+    });
+
+    await docClient.send(putCommand);
+
+    return {
+      statusCode: 201,
+      headers: RESPONSE_HEADERS,
+      body: ''
+    };
+  }
 };
 
 exports.handler = handler;
--- src/pages/engine/useRolls.tsx
+++ src/pages/engine/useRolls.tsx
@@ -2,8 +2,9 @@ import React, { ReactNode, useContext } from 'react';
 import { useLocalStorage } from '@/utils/useLocalStorage';
 import { useStateVersioner } from '@/utils/useStateVersioner';
 import { useSession } from './useSession';
-import { useQuery } from 'react-query';
+import { useMutation, useQuery } from 'react-query';
 import { v4 as uuid } from 'uuid';
+import { DateTime } from 'luxon';
 
 export enum RollEvaluation {
   CHECK = 'CHECK',
@@ -63,24 +64,43 @@ const MAX_ROLLS = 100;
 
 export const useRolls = () => {
   const rollsState = useContext(RollsStateContext);
-  const { sessionId } = useSession();
+  const { sessionId, session } = useSession();
 
   const { data: sessionRolls } = useQuery<Roll[]>(
     ['GET_SESSION_ROLLS'],
     () =>
-      fetch(`https://api.vademecum.thenjk.com/sessions/${sessionId}/rolls`).then(response =>
+      fetch(`https://api.vademecum.thenjk.com/sessions/${session!.id}/rolls`).then(response =>
         response.json()
       ),
     {
-      enabled: !!sessionId
+      enabled: !!session
     }
   );
 
+  const createSessionRoll = useMutation((roll: Roll) => {
+    return fetch(`https://api.vademecum.thenjk.com/sessions/${sessionId}/rolls`, {
+      method: 'POST',
+      mode: 'no-cors',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(roll)
+    }).then(response => response.json());
+  });
+
   const addRoll = (roll: Omit<Roll, 'id'>) => {
-    rollsState.update({
-      rolls: [{ id: uuid(), ...roll }, ...rollsState.rolls].slice(0, MAX_ROLLS)
-    });
+    const newRoll = { id: uuid(), ...roll };
+    if (session) {
+      createSessionRoll.mutate(newRoll);
+    } else {
+      rollsState.update({
+        rolls: [...rollsState.rolls, newRoll].slice(0, MAX_ROLLS)
+      });
+    }
   };
+
+  const rolls = sessionId ? sessionRolls ?? [] : rollsState.rolls;
+  rolls.sort((a, b) => (DateTime.fromISO(b.timestamp) > DateTime.fromISO(a.timestamp) ? 1 : -1));
 
   return {
     rolls: sessionId ? sessionRolls ?? [] : rollsState.rolls,
