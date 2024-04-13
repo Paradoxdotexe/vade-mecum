ac6f2851560ccc8d1967835f6001a62acf06d1d6
Nathan Klingensmith <paradoxpyt@gmail.com>
2024-04-12 21:47:07 -0400

    PATCH: Add lambda types

--- aws/lambda/getSessions/index.ts
+++ aws/lambda/getSessions/index.ts
@@ -1,6 +1,10 @@
-exports.handler = async event => {
+import { APIGatewayProxyHandler } from 'aws-lambda';
+
+const handler: APIGatewayProxyHandler = async (event, context) => {
   return {
     statusCode: 404,
     body: 'No sessions found.'
   };
 };
+
+exports.handler = handler;
