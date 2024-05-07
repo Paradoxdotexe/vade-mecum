d4e9c4fa33dfac1ee4131974d67588062f50e80d
Nathan Klingensmith <paradoxpyt@gmail.com>
2024-05-06 22:36:14 -0400

    PATCH: Remove mocks

--- src/pages/vtt/sessions/SessionsPage.tsx
+++ src/pages/vtt/sessions/SessionsPage.tsx
@@ -18,14 +18,7 @@ const StyledSessionsPage = styled(PageLayout)`
 `;
 
 export const SessionsPage: React.FC = () => {
-  const sessions: Session[] = [
-    {
-      id: '34d0990a-1c81-4545-b6c0-cae3bc68a59c',
-      userId: 'ec3300eb-d45e-4865-b152-6131cbd1d5bc',
-      name: "The World's End",
-      characterIds: ['a3efa38b-a2e4-4446-94c8-448456e9549c', 'a3efa38b-a2e4-4446-94c8-448456e9549c']
-    }
-  ];
+  const sessions: Session[] = [];
 
   return (
     <StyledSessionsPage>
--- src/pages/vtt/sessions/session/SessionPage.tsx
+++ src/pages/vtt/sessions/session/SessionPage.tsx
@@ -14,12 +14,7 @@ export const SessionPage: React.FC = () => {
   const theme = useVTheme();
   //const { sessionId } = useParams();
 
-  const session: Session = {
-    id: '34d0990a-1c81-4545-b6c0-cae3bc68a59c',
-    userId: 'ec3300eb-d45e-4865-b152-6131cbd1d5bc',
-    name: "The World's End",
-    characterIds: ['a3efa38b-a2e4-4446-94c8-448456e9549c', 'a3efa38b-a2e4-4446-94c8-448456e9549c']
-  };
+  const session = undefined as Session | undefined;
 
   return (
     <StyledSessionPage>
